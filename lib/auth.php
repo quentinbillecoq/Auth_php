d7d92ea8088e9247d591eb88542555189aeeecc1
Arkadia977 <arkadia977@hotmail.fr>
2017-07-25 22:19:35 +0200

    Update auth.php

--- lib/auth.php
+++ lib/auth.php
@@ -107,7 +107,7 @@ class Auth{
 				return Array('validatorId'=>true,'error'=>'');
 
 			}else{
-
+ 
 				return Array('validatorId'=>false,'error'=>'Wrong login');
 
 			}
@@ -121,61 +121,57 @@ class Auth{
 		return array('connection'=>true,'error'=>'');
 	}
 
-	private function _32Decode($secret)
-    {
-        if (empty($secret)) { return ''; }
-        $base32chars = $this->base32Table;
-        $base32charsFlipped = array_flip($base32chars);
-        $paddingCharCount = substr_count($secret, $base32chars[32]);
-        $allowedValues = array(6, 4, 3, 1, 0);
-        if (!in_array($paddingCharCount, $allowedValues)) { return false;}
-        for ($i = 0; $i < 4; ++$i) { if ($paddingCharCount == $allowedValues[$i] && substr($secret, -($allowedValues[$i])) != str_repeat($base32chars[32], $allowedValues[$i])) {return false;}}
-        $secret = str_replace('=', '', $secret);
-        $secret = str_split($secret);
-        $binaryString = '';
-        for ($i = 0; $i < count($secret); $i = $i + 8) {$x = ''; if (!in_array($secret[$i], $base32chars)) {return false;} for ($j = 0; $j < 8; ++$j) { $x .= str_pad(base_convert(@$base32charsFlipped[@$secret[$i + $j]], 10, 2), 5, '0', STR_PAD_LEFT);} $eightBits = str_split($x, 8); for ($z = 0; $z < count($eightBits); ++$z) {$binaryString .= (($y = chr(base_convert($eightBits[$z], 2, 10))) || ord($y) == 48) ? $y : '';}}
-        return $binaryString;
-    }
+	private function _32Decode($secret){
+		if (empty($secret)) { return ''; }
+		$base32chars = $this->base32Table;
+		$base32charsFlipped = array_flip($base32chars);
+		$paddingCharCount = substr_count($secret, $base32chars[32]);
+		$allowedValues = array(6, 4, 3, 1, 0);
+		if (!in_array($paddingCharCount, $allowedValues)) { return false;}
+		for ($i = 0; $i < 4; ++$i) { if ($paddingCharCount == $allowedValues[$i] && substr($secret, -($allowedValues[$i])) != str_repeat($base32chars[32], $allowedValues[$i])) {return false;}}
+		$secret = str_replace('=', '', $secret);
+		$secret = str_split($secret);
+		$binaryString = '';
+		for ($i = 0; $i < count($secret); $i = $i + 8) {$x = ''; if (!in_array($secret[$i], $base32chars)) {return false;} for ($j = 0; $j < 8; ++$j) { $x .= str_pad(base_convert(@$base32charsFlipped[@$secret[$i + $j]], 10, 2), 5, '0', STR_PAD_LEFT);} $eightBits = str_split($x, 8); for ($z = 0; $z < count($eightBits); ++$z) {$binaryString .= (($y = chr(base_convert($eightBits[$z], 2, 10))) || ord($y) == 48) ? $y : '';}}
+		return $binaryString;
+	}
 
 	private function getCode($secret, $timeSlice = null)
-    {
-        if ($timeSlice === null) {$timeSlice = floor(time() / 30);}
-        $secretkey = $this->_32Decode($secret);
-        $codeLength = 6;
-        $time = chr(0).chr(0).chr(0).chr(0).pack('N*', $timeSlice);
-        $hm = hash_hmac('SHA1', $time, $secretkey, true);
-        $offset = ord(substr($hm, -1)) & 0x0F;
-        $hashpart = substr($hm, $offset, 4);
-        $value = unpack('N', $hashpart);
-        $value = $value[1];
-        $value = $value & 0x7FFFFFFF;
-        $modulo = pow(10, $codeLength);
-        return str_pad($value % $modulo, $codeLength, '0', STR_PAD_LEFT);
-    }
-    private function timecode($s, $u)
-    {
-        if (function_exists('hash_equals')) { return hash_equals($s, $u); }
-        $safeLen = strlen($s);
-        $userLen = strlen($u);
-        if ($userLen != $safeLen) { return false; }
-        $result = 0;
-        for ($i = 0; $i < $userLen; ++$i) {
-            $result |= (ord($s[$i]) ^ ord($u[$i]));
-        }
-        return $result === 0;
-    }
-    public function validatorOTP($secret, $code, $aro = 1)
-    {
-
-        if (strlen($code) != 6) { return false; }
-        for ($i = -$aro; $i <= $aro; ++$i) {
-            $calculatedCode = $this->getCode($secret, floor(time() / 30) + $i);
-            if ($this->timecode($calculatedCode, $code)) {
-                return true;
-            }
-        }
-        return false;
-    }
+	{
+		if ($timeSlice === null) {$timeSlice = floor(time() / 30);}
+		$secretkey = $this->_32Decode($secret);
+		$codeLength = 6;
+		$time = chr(0).chr(0).chr(0).chr(0).pack('N*', $timeSlice);
+		$hm = hash_hmac('SHA1', $time, $secretkey, true);
+		$offset = ord(substr($hm, -1)) & 0x0F;
+		$hashpart = substr($hm, $offset, 4);
+		$value = unpack('N', $hashpart);
+		$value = $value[1];
+		$value = $value & 0x7FFFFFFF;
+		$modulo = pow(10, $codeLength);
+		return str_pad($value % $modulo, $codeLength, '0', STR_PAD_LEFT);
+	}
+	private function timecode($s, $u){
+		if (function_exists('hash_equals')) { return hash_equals($s, $u); }
+		$safeLen = strlen($s);
+		$userLen = strlen($u);
+		if ($userLen != $safeLen) { return false; }
+		$result = 0;
+		for ($i = 0; $i < $userLen; ++$i) {
+		    $result |= (ord($s[$i]) ^ ord($u[$i]));
+		}
+		return $result === 0;
+	}
+	public function validatorOTP($secret, $code, $aro = 1){
+		if(strlen($code) != 6) { return false; }
+		for ($i = -$aro; $i <= $aro; ++$i) {
+			$calculatedCode = $this->getCode($secret, floor(time() / 30) + $i);
+			if ($this->timecode($calculatedCode, $code)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	public function connection($user, $password, $otp=false, $hash=null, $iter=null, $booldel=false){
 		if($hash === null){ $hash=$this->hashSalt; }
